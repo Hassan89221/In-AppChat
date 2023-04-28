6dfa296dc37efaa2533e174ab2ccc8e9a722cb4d
Hassan89221 <iceblue89221@gmail.com>
2023-04-28 23:33:22 +0500

    First commit

---
+++ src/assets/js/config.js
@@ -0,0 +1,7 @@
+export const avatarPrefix = "https://storage.zego.im/zim/example/web/assets/"
+
+export const appConfig = {
+  appID: 1803466597, // AppID - type: number
+  appSign: "f0ddc82b66e6ff93e1eee62720bbd54feb3b0cc1c5ac13a82fcd85168d69ad3c", // AppSign - type: 64 byte length string
+}
+
